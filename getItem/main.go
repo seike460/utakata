9176601a8dad59e1b3e736dd3b65cead80e0ea68
seike460 <seike460@gmail.com>
2018-03-17 11:20:49 +0900

    :+1: Jquery版をReact版に修正しようと試みる前

---
+++ getItem/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	svc := dynamodb.New(session.New())
+	input := &dynamodb.ScanInput{
+		TableName: aws.String("task"),
+	}
+
+	result, err := svc.Scan(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+	}
+	jsonString, err := json.Marshal(result.Items)
+	fmt.Println(jsonString)
+
+	return events.APIGatewayProxyResponse{Body: string(jsonString), StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
